a84654ce5b669c0ba234846ea4a5e85b572c9313
Lukas <lms21a@fsu.edu>
2023-06-29 18:08:12 -0500

    New Scheduler + implementation

--- src/training/train_model.py
+++ src/training/train_model.py
@@ -1,81 +1,107 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from ..tools import convert_readable
-import wandb 
+import os
+from tqdm.auto import tqdm
+from ..training.schedulers import CustomCosineAnnealingWarmupScheduler
 
-criterion = nn.CrossEntropyLoss()
+class Trainer:
+    def __init__(self, model, config_file, device, train_loader, val_loader):
+        self.model = model
+        self.config = config_file
+        self.device = device
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = self.initialize_optimizer()
+        self.train_losses = []
+        self.val_losses = []
+        self.generations = []
 
-# def initialize_wandb(model, model_config):
-#     run = wandb.init(project='SynthAI', config=model_config)
-#     wandb.watch(model, log='all')
-#     return run
+    def initialize_optimizer(self):
+        return torch.optim.Adam(self.model.parameters(), lr=self.config.learning_rate)
 
-def initialize_optimizer(model, learning_rate):
-    return torch.optim.Adam(model.parameters(), lr=learning_rate)
+    def move_to_device(self, inputs, targets):
+        inputs = inputs.to(self.device)
+        targets = targets.to(self.device)
+        return inputs, targets
 
-def move_to_device(inputs, targets, device):
-    inputs = inputs.to(device)
-    targets = targets.to(device)
-    return inputs, targets
+    def compute_loss(self, outputs, targets):
+        outputs = outputs.view(-1, outputs.shape[-1])
+        loss = self.criterion(outputs, targets.view(-1))
+        return loss
 
-def compute_loss(outputs, targets, criterion):
-    outputs = outputs.view(-1, outputs.shape[-1])
-    loss = criterion(outputs, targets.view(-1))
-    return loss
+    def do_backprop(self, loss, step):
+        loss = loss / self.config.grad_accum_steps  # Normalize our loss (if averaged)
+        loss.backward()
+        if (step+1) % self.config.grad_accum_steps == 0:
+            self.optimizer.step()
+            self.optimizer.zero_grad()
 
-def do_backprop(loss, optimizer):
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    def save_checkpoint(self, step):
+            """Save a training checkpoint."""
 
-def train_step(model, train_loader, optimizer, criterion, device):
-    model.train()
-    inputs, targets = next(iter(train_loader))
-    inputs, targets = move_to_device(inputs, targets, device)
-    outputs = model(inputs)
-    loss = compute_loss(outputs, targets, criterion)
-    do_backprop(loss, optimizer)
-    return loss.item()
+            checkpoint = {
+                'model_state_dict': self.model.state_dict(),
+                'optimizer_state_dict': self.optimizer.state_dict(),
+                'step': step,
+                'train_losses': self.train_losses,
+                'val_losses': self.val_losses
+                # Add any other state information you want to preserve
+            }
+            
+            # You can organize your checkpoints in subdirectories if you want
+            checkpoint_dir = os.path.join("checkpoints", f"checkpoint_{step}.pt")
+            torch.save(checkpoint, checkpoint_dir)
+            print(f"Saved checkpoint at step {step}.")
 
-def val_step(model, val_loader, criterion, device):
-    model.eval()
-    with torch.no_grad():
-        inputs, targets = next(iter(val_loader))
-        inputs, targets = move_to_device(inputs, targets, device)
-        outputs = model(inputs)
-        generations = convert_readable(model.generate(inputs,100))
-        loss = compute_loss(outputs, targets, criterion)
-        return loss.item(), generations
+    def train_step(self, step):
+        self.model.train()
+        inputs, targets = next(iter(self.train_loader))
+        inputs, targets = self.move_to_device(inputs, targets)
+        outputs = self.model(inputs)
+        loss = self.compute_loss(outputs, targets)
+        self.do_backprop(loss, step)
+        return loss.item()
 
-def train_model(model, train_loader, val_loader, config_file, device):
-    config = config_file
-    
-    learning_rate = config.learning_rate 
-    max_steps = config.max_steps 
-    val_interval = config.val_interval
+    def val_step(self):
+        self.model.eval()
+        with torch.no_grad():
+            inputs, targets = next(iter(self.val_loader))
+            inputs, targets = self.move_to_device(inputs, targets)
+            outputs = self.model(inputs)
+            generations = convert_readable(self.model.generate(inputs, 100))
+            loss = self.compute_loss(outputs, targets)
+            return loss.item(), generations
 
-    optimizer = initialize_optimizer(model, learning_rate)
+    def train(self):
+            step = 0 
 
-    train_losses = []
-    val_losses = []
-    generations = []
-    step = 0 
-    while True:
-        train_loss = train_step(model, train_loader, optimizer, criterion, device)
-        train_losses.append(train_loss)
-        print(f"Step {step} | Training loss: {train_loss}")
-        
-        if step % val_interval == 0:
-            val_loss, gen = val_step(model, val_loader, criterion, device)
-            val_losses.append(val_loss)
-            generations.append(gen)
-            print(f"Step {step} | Validation loss: {val_loss}")
-        
-        step += 1
-        if step >= max_steps: break
-    
-    with open("outputs/generations.txt", "w") as f:
-        f.writelines(gen + "\n" for generated in generations for gen in generated)
+            # Initialize a tqdm progress bar
+            pbar = tqdm(total=self.config.max_steps, desc="Training Progress")
 
-    return train_losses, val_losses
+            while True:
+                train_loss = self.train_step(step)
+                self.train_losses.append(train_loss)
+
+                if step % self.config.val_interval == 0:
+                    val_loss, gen = self.val_step()
+                    self.val_losses.append(val_loss)
+                    self.generations.append(gen)
+                    pbar.set_postfix({"Training Loss": train_loss, "Validation Loss": val_loss})
+
+                # If checkpoint_interval is not 0, save a checkpoint every checkpoint_interval steps
+                if self.config.checkpoint_interval and step % self.config.checkpoint_interval == 0:
+                    self.save_checkpoint(step)
+                
+                step += 1
+                pbar.update(1)  # update the progress bar
+
+                if step >= self.config.max_steps: 
+                    pbar.close()
+                    break
+
+            with open("outputs/generations.txt", "w") as f:
+                f.writelines(gen + "\n" for generated in self.generations for gen in generated)
+
+            return self.train_losses, self.val_losses
