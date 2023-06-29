Fixed Source Leakage

src/setup_data.py
@@ -2,9 +2,7 @@ import torch
 import tiktoken
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data import Dataset
-from unit_tests import check_for_causal
 
-UNIT_TEST = False
 class CausalDataset(Dataset):
     def __init__(self, data, context_length):
         self.data = data
@@ -35,7 +33,7 @@ class CausalDataset(Dataset):
 
 def preprocess(context_length,batch_size,shuffle):
     enc= tiktoken.encoding_for_model('gpt2')
-    with open('synthai/data/shakespeare.txt', 'r') as file:
+    with open('data/shakespeare.txt', 'r') as file:
         text = file.read()
 
     data = torch.tensor(enc.encode(text), dtype=torch.long)
@@ -43,10 +41,6 @@ def preprocess(context_length,batch_size,shuffle):
 
     dataset = CausalDataset(data, context_length)
     train_loader,val_loader,test_loader= dataset.return_dataloaders(batch_size=batch_size,shuffle=shuffle)
-    if UNIT_TEST:
-        check_for_causal(train_loader)
-        check_for_causal(val_loader)
-        check_for_causal(test_loader)
 
     return train_loader,val_loader,test_loader
 

src/training/train_model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from synthai.src.tools import convert_readable
+from ..tools import convert_readable 
 
 criterion = nn.CrossEntropyLoss()
 
@@ -70,7 +70,7 @@ def train_model(model, train_loader, val_loader, config_file, device):
     
 
 
-    with open("synthai/outputs/generations.txt", "w") as f:
+    with open("outputs/generations.txt", "w") as f:
         f.writelines(gen + "\n" for generated in generations for gen in generated)
         f.write("-" * 50 + "\n")
 