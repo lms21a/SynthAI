9281764442c7f220f857861b4065cfe746456dd6
Lukas <lms21a@fsu.edu>
2023-07-28 23:27:47 -0500

    Refactor: allow presharding of data

--- src/load_tokenize.py
+++ src/load_tokenize.py
@@ -6,13 +6,16 @@ import os
 import gzip
 import shutil
 
-dataset = 'roneneldan/TinyStories'
+dataset = 'bigcode/the-stack-dedup'
+data_dir = 'data/python'
+dataset_idx = 'content'
 cache_dir = '/mnt/d/hf_cache/'
-save_dir = '/mnt/d/benchmarks/tinystories/'
-test_size = 0.1
+save_dir = '/mnt/d/benchmarks/stack_python/'
+test_size = 0.0005
 enc = tiktoken.get_encoding('gpt2')
 max_rows = 10000 #shard size
-compression = False 
+compression = False
+iters = 4
 
 def create_file(file_dir, file_name):
     if not os.path.exists(file_dir):
@@ -20,7 +23,7 @@ def create_file(file_dir, file_name):
     return os.path.join(file_dir, file_name)
 
 def tokenize_data(data):
-    ids = enc.encode_ordinary(data['text'])
+    ids = enc.encode(data[dataset_idx],allowed_special='all')
     ids.append(enc.eot_token)
     output = {'tokens': ids, 'len': len(ids)}
     return output
@@ -58,37 +61,39 @@ def decompress_folder(directory):
             os.remove(file_path)
     print("Decompressed All Files")
 
-ds = load_dataset(dataset,cache_dir=cache_dir)
-split_dataset = ds['train'].train_test_split(test_size=test_size)
-preprocessed_dataset = split_dataset.map(
-    tokenize_data,
-    remove_columns=['text'],
-    desc='Tokenizing',
-    num_proc=12
-)
+full_ds = load_dataset(dataset,data_dir=data_dir,cache_dir=cache_dir)
 
-info = {'train':None,'test':None}
+for i in range(iters):
+    ds = full_ds['train'].shard(iters,i)
+    split_dataset = ds.train_test_split(test_size=test_size)
+    preprocessed_dataset = split_dataset.map(
+        tokenize_data,
+        remove_columns=[dataset_idx],
+        desc='Tokenizing',
+        num_proc=8
+    )
+    info = {'train':None,'test':None}
+    for split, ds in preprocessed_dataset.items(): 
+        max_len = np.sum(ds['len'])
+        info[split] = f'Total Tokens: {max_len}'        
+        filename = create_file(save_dir,f'{split}_{i}.tokens')
+        data = np.memmap(filename,mode='w+',shape=(max_len,))
+        num_shards = ds.num_rows // max_rows
 
-for split, ds in preprocessed_dataset.items(): 
-    max_len = np.sum(ds['len'])
-    info[split] = f'Total Tokens: {max_len}'        
-    filename = create_file(save_dir,f'{split}.tokens')
-    data = np.memmap(filename,mode='w+',shape=(max_len,))
-    num_shards = ds.num_rows // max_rows
+        idx = 0
+        for s in tqdm(range(num_shards)):
+            shard = ds.shard(num_shards=num_shards,index=s,contiguous=True).with_format('numpy')
+            data_shard = np.concatenate(shard['tokens'])
+            data[idx:idx + len(data_shard)] = data_shard
+            idx += len(shard['tokens'])
 
-    idx = 0
-    for s in tqdm(range(num_shards)):
-        shard = ds.shard(num_shards=num_shards,index=s,contiguous=True).with_format('numpy')
-        data_shard = np.concatenate(shard['tokens'])
-        data[idx:idx + len(data_shard)] = data_shard
-        idx += len(shard['tokens'])
+        data.flush()
+        # TODO: Create a sole info file
+    info_file = os.path.join(save_dir, f'info{i}.txt')
 
-    data.flush()
-
+    with open(info_file, 'w') as f:
+        for split in ['train','test']:
+            f.write(f'Split: {split}_{i}\n\t{info[split]}_{i}\n')
+        f.write('Compression: True\n' if compression else 'Compression: False\n')
+    
 compress_folder(save_dir) if compression else None
-
-info_file = os.path.join(save_dir, 'info.txt')
-with open(info_file, 'w') as f:
-    for split in ['train','test']:
-        f.write(f'Split: {split}\n\t{info[split]}\n')
-    f.write('Compression: True\n' if compression else 'Compression: False\n')
