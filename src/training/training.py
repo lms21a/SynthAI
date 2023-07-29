160a1d95fc248c11741746f67b0eb51dbc1a1051
Lukas <lms21a@fsu.edu>
2023-07-28 23:46:39 -0500

    Refactor: Training Script v3

--- src/training/training.py
+++ src/training/training.py
@@ -1,46 +1,32 @@
 import torch
 import numpy as np
-from datasets import load_dataset
+import torch.nn as nn
+import torch.nn.functional as F
 from dataclasses import dataclass
-import tiktoken
+from ..models.gpt_torch import GPT_torch, GPT_Config
+from .schedulers import adaptive_momentum_scheduler, update_grad
 
 @dataclass
 class TrainArgs:
-    test_size: float = 0.005
-    tokenizer: str = 'gpt2'
-    cntx: int = 64
-    btch: int = 64
+    lr_init = .1
+    momentum = 0
+    max_steps = 5000
+    batch_size = 4
+    eval_interval = 500
+    step = 0
+    warm_up_steps = 100
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+model_config = GPT_Config(
+    vocab_size=50257,
+    d_model = 32,
+    cntx_len=32,
+    n_layers=2,
+    n_head=4
+)    
 
-
-
-configs = TrainArgs()
-# Preprocessing of data
-
-ds = load_dataset("Skylion007/openwebtext",cache_dir='/mnt/d/benchmarks')
-split_dataset = ds['train'].train_test_split(test_size=configs.test_size)
-train_dataset = split_dataset['train']
-test_dataset = split_dataset['test']
-
-# datasharding
-def shard_dataset(dataset, num_shards):
-    for i in range(num_shards):
-        yield dataset.shard(num_shards, i)
-
-shard_gen = shard_dataset(train_dataset, 500)
-
-# tokenization
-enc = tiktoken.encoding_for_model(configs.tokenizer)
-
-def tokenize_shard(shard):
-    tokens = [enc.encode_batch(data['text']) for data in shard]
-    return tokens
-
-tokenized_shard = tokenize_shard(next(shard_gen))
-
-
-# Get a batch of data
-def get_batch(data,cntx,btch):
+def get_batch(split,btch=TrainArgs.batch_size,cntx=model_config.cntx_len,device=TrainArgs.device):
+    data = train if split == 'train' else test 
     len_data = len(data)-cntx  
 
     # Generate all starting indices at once
@@ -51,6 +37,22 @@ def get_batch(data,cntx,btch):
     
     return x, y
 
-print(len(tokenized_shard))
 
+data = np.memmap('lexpods.bin', mode='r')
+train = data[:int(.9*len(data))]
+test = data[int(.9*len(data)):]
+
+
+
+
+
+@torch.inference_mode()
+def eval_losses(iters):
+    losses = []
+    for _ in range(iters):
+        x,y = get_batch('valid',TrainArgs.batch_size,model_config.cntx_len)
+        loss,_ = model(x,y)
+        losses.append(loss.item())
+    avg_loss = sum(losses) / len(losses)
+    return avg_loss
 
